b63da8116b0f0b1f95dfd870dd1fd5c08fb30b8e
ViktorSorokin7500 <berhaneselassie1945@gmail.com>
2025-03-24 16:59:43 +0200

    i dont know what i do

--- app/api/test-db/route.ts
+++ app/api/test-db/route.ts
@@ -4,9 +4,7 @@ import { initDb, sql } from "@/lib/db";
 export async function GET() {
   try {
     await initDb();
-    await sql`INSERT INTO users (clerk_id, tokens) VALUES ('test_clerk_id_123', 20000) ON CONFLICT (clerk_id) DO NOTHING;`;
-    const result =
-      await sql`SELECT * FROM users WHERE clerk_id = 'test_clerk_id_123'`;
+    const result = await sql`SELECT * FROM users`; // Показуємо всіх юзерів
     return NextResponse.json({ success: true, data: result });
   } catch (error) {
     return NextResponse.json(
