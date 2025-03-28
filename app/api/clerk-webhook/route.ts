d061c3e4d57ed35971882042ddb854900d2c5213
ViktorSorokin7500 <berhaneselassie1945@gmail.com>
2025-03-28 17:08:37 +0200

    qwerqwer

--- app/api/clerk-webhook/route.ts
+++ app/api/clerk-webhook/route.ts
@@ -8,43 +8,22 @@ export async function POST(request: Request) {
 
   const wh = new Webhook(process.env.CLERK_WEBHOOK_SECRET || "");
   try {
-    // Проверяем валидность вебхука
+    // Перевіряємо, чи валідний Webhook
     wh.verify(JSON.stringify(payload), {
       "svix-id": headers.get("svix-id") || "",
       "svix-timestamp": headers.get("svix-timestamp") || "",
       "svix-signature": headers.get("svix-signature") || "",
     });
 
-    // Обрабатываем создание пользователя
+    // Якщо юзер створений, додаємо його в БД
     if (payload.type === "user.created") {
       const clerkId = payload.data.id;
-      const email = payload.data.email_addresses[0].email_address; // Основная почта из Clerk
-
-      // Проверяем, есть ли пользователь с таким email
-      const existingUser = await sql`
-        SELECT * FROM users WHERE email = ${email} LIMIT 1;
+      await sql`
+        INSERT INTO users (clerk_id, tokens)
+        VALUES (${clerkId}, 20000)
+        ON CONFLICT (clerk_id) DO NOTHING;
       `;
-
-      if (existingUser.length > 0) {
-        // Если пользователь уже есть, обновляем clerk_id, токены остаются прежними
-        await sql`
-          UPDATE users
-          SET clerk_id = ${clerkId}
-          WHERE email = ${email};
-        `;
-        console.log(
-          `User with email ${email} updated with new clerk_id ${clerkId}`
-        );
-      } else {
-        // Если пользователя нет, создаём нового с 20000 токенов
-        await sql`
-          INSERT INTO users (clerk_id, email, tokens)
-          VALUES (${clerkId}, ${email}, 20000);
-        `;
-        console.log(
-          `New user ${clerkId} with email ${email} added with 20000 tokens`
-        );
-      }
+      console.log(`User ${clerkId} added with 20000 tokens`);
     }
 
     return NextResponse.json({ success: true });
